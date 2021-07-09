acc8ef94d9734af67d8bc0cd09ba1850aea8a760
bhimantoro <bhimantoros@gmail.com>
2021-07-09 19:06:39 +0700

    env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://b14cf88b275787:96653b0d@us-cdbr-east-04.cleardb.com/heroku_44b86e966b9e231'
+DB_USERNAME = config('DB_USERNAME')
+DB_HOST = config('DB_HOST')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_NAME = config('DB_NAME')
+
+SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://' + \
+    DB_USERNAME+':'+DB_PASSWORD+'@'+DB_HOST+'/'+DB_NAME
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
