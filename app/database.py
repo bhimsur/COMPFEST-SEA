f0b429f8d8d30754fb480787b006c627395803f2
bhimantoro <bhimantoros@gmail.com>
2021-07-09 18:01:30 +0700

    remote sql

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://root@localhost:3306/compfest'
+SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://b14cf88b275787:96653b0d@us-cdbr-east-04.cleardb.com/heroku_44b86e966b9e231'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
